71f22a2ff44203cf81641566f0644ed45e23374e
bgschust <61630536+bgschust@users.noreply.github.com>
2023-01-13 19:35:08 -0500

    Add files via upload

--- cropify/components/functions.js
+++ cropify/components/functions.js
@@ -15,3 +15,28 @@ function buildURL(baseURL, queryParams) {
   let url = baseURL + query;
   return url;
 }
+
+function setCookie(name, value, expDays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (expDays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = name + "=" + value + ";" + expires + ";path=/";
+  alert(document.cookie);
+}
+
+function getCookie(name) {
+  // Split cookie string and get all individual name=value pairs in an array
+  var cookieArr = document.cookie.split(";");
+  // Loop through the array elements
+  for(var i = 0; i < cookieArr.length; i++) {
+      var cookiePair = cookieArr[i].split("=");
+      /* Removing whitespace at the beginning of the cookie name
+      and compare it with the given string */
+      if(name == cookiePair[0].trim()) {
+          // Decode the cookie value and return
+          return decodeURIComponent(cookiePair[1]);
+      }
+  }
+  // Return null if not found
+  return null;
+} // Src: https://www.tutorialrepublic.com/javascript-tutorial/javascript-cookies.php
